f360fcf4aebb9e836db4e3c69d437bf2cc7d6063
tarun_72 <tarunshrm395@gmail.com>
2021-09-05 18:09:02 +0530

    1. added informations

--- app/src/main/java/com/breakingbad/trial/base/BreakingBadApplication.kt
+++ app/src/main/java/com/breakingbad/trial/base/BreakingBadApplication.kt
@@ -3,10 +3,11 @@ package com.breakingbad.trial.base
 import com.breakingbad.trial.base.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
-
+/**
+ * Inject android applications
+ * */
 class BreakingBadApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().application(this).build()
     }
-
 }
--- app/src/main/java/com/breakingbad/trial/base/di/AppComponent.kt
+++ app/src/main/java/com/breakingbad/trial/base/di/AppComponent.kt
@@ -7,14 +7,21 @@ import dagger.Component
 import dagger.android.AndroidInjectionModule
 import dagger.android.AndroidInjector
 import javax.inject.Singleton
-
+/**
+ * this interface is important for dagger application init
+ *
+ * */
 @Singleton
 @Component(modules = [
-    AndroidInjectionModule::class
+    AndroidInjectionModule::class // Dagger class to add Base applications
 ])
-interface AppComponent : AndroidInjector<BreakingBadApplication>{
+interface AppComponent : AndroidInjector<BreakingBadApplication>{  // inject our base class
     override fun inject(application: BreakingBadApplication)
-
+    /**
+     * A builder for a component.
+     * Components may have a single nested static abstract class or interface annotated with @Component.Builder.
+     * If they do, then the component's generated builder will match the API in the type.
+     * */
     @Component.Builder
     interface Builder{
 
